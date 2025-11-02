f742be92a84c19dc74a3451b95f30d9371782b7b
fraulueneburg <github@fraulueneburg.de>
2025-11-02 10:30:16 +0100

    refactor (popup): clean up naming, add total number of items

--- popup.js
+++ popup.js
@@ -4,13 +4,18 @@ document.getElementById('calc').addEventListener('click', async () => {
 	const [{ result }] = await chrome.scripting.executeScript({
 		target: { tabId: tab.id },
 		func: () => {
-			const foo = Array.from(document.querySelectorAll('[itemprop="price"]'))
+			const listItems = Array.from(document.querySelectorAll('[itemprop="price"]'))
 				.map((el) => el.getAttribute('content'))
 				.map(Number)
 				.filter((n) => !isNaN(n))
-			return foo.reduce((acc, curr) => acc + curr, 0)
+
+			const sum = listItems.reduce((acc, curr) => acc + curr, 0)
+			return {
+				sumFormatted: sum.toLocaleString(undefined, { style: 'currency', currency: 'EUR' }),
+				count: listItems.length,
+			}
 		},
 	})
 
-	document.getElementById('result').textContent = `Sum: ${result}`
+	document.getElementById('result').textContent = `Sum: ${result.sumFormatted} (${result.count} items)`
 })
